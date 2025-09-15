8667aa92d164cadd45add97f9d80cb2e18cb6c41
AlejandroMora05 <a.chavarria@udea.edu.co>
2025-09-15 10:43:58 -0500

    Lab1_25-2

---
+++ src/test/java/com/udea/lab_1/__2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.lab_1.__2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
